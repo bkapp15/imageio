9b52a70e4110c1608902bc3501ee4f468b0f86c7
bkapp15 <kapp1040@gmail.com>
2022-01-23 20:25:21 -0600

    added sass support

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-// import '../styles/Home.css';
 import Head from 'next/head'
 import ImageGrid from "../components/ImageGrid";
 import UploadButton from "../components/UploadButton";
